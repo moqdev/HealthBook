4a17b118390ab173a1d6df3b07a1fdeadb9b41cd
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-08-31 18:59:10 -0400

    Diagnosis and prescription

---
+++ frontend/src/Diagnose.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  Box,
+  Button,
+  Heading,
+  Form,
+  TextArea,
+  Grommet
+} from 'grommet';
+import './App.css';
+const theme = {
+  global: {
+    colors: {
+      brand: '#000000',
+      focus: "#000000",
+      active: "#000000",
+    },
+    font: {
+      family: 'Lato',
+    },
+  },
+};
+var diagnosis;
+var prescription;
+var id;
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    style={{ zIndex: '1' }}
+    {...props} />
+);
