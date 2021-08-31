7b50001eeaa10289909327f7959cefb2b36a05fd
Mona Waseem <monaqwaseem@gmail.com>
2021-08-31 23:36:06 +0000

    Create ViewOneHistory component

---
+++ frontend/src/ViewOneHistory.js
@@ -0,0 +1,29 @@
+import React, { Component} from 'react';
+
+import {
+    Box,
+    Heading,
+    Grommet,
+    Table,
+    TableBody,
+    TableCell,
+    TableRow
+} from 'grommet';
+
+import './App.css';
+
+const theme = {
+    global: {
+        colors: {
+            brand: '#000000',
+            focus: '#000000'
+        },
+        font: {
+            family: 'Lato',
+        },
+    },
+};
+
+
+
+export default ViewOneHistory;
