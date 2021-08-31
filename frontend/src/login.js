b2bd4d9f05190f6721bd1e3d9563e58ce8f0be68
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-08-31 16:39:14 -0400

    Resolving conflicts

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
+import { withRouter } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Heading,
+  Grommet,
+  FormField,
+  Form,
+  CheckBox,
+} from 'grommet';
+
 import './App.css';
 
 class LogIn extends Component {
   state = { isDoctor: false }
-
 
 }
