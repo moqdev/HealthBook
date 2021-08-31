97a249cf40d9145397cd4862adae6cfac2a2cb5e
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-08-31 19:42:01 -0400

    Doctor home

---
+++ frontend/src/DocHome.js
@@ -0,0 +1,23 @@
+import React, { Component, useState } from 'react';
+import {
+    Box,
+    Button,
+    Heading,
+    Grommet,
+    Grid,
+    Text,
+} from 'grommet';
+
+import './App.css';
+
+const theme = {
+    global: {
+        colors: {
+            brand: '#000000',
+            focus: '#000000'
+        },
+        font: {
+            family: 'Lato',
+        },
+    },
+};
