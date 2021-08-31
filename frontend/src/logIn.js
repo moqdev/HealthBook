9f6fe37056bb07974a4ecb3324c53f5599068731
Mona Waseem <monaqwaseem@gmail.com>
2021-08-31 18:50:45 +0000

    Build LogIn

--- frontend/src/logIn.js
+++ frontend/src/logIn.js
@@ -40,5 +40,30 @@ const AppBar = (props) => (
 class LogIn extends Component {
   state = { isDoctor: false }
 
+  constuctor() {
+    this.routeChange = this.routeChange.bind(this);
+  }
 
+  routeChange() {
+    let path = '/Home';
+    this.props.history.push(path);
+  }
+
+  render() {
+    const { isDoctor } = this.state; // In order to query from doctor's table
+
+    return (
+      <Grommet theme={theme} full>
+        <AppBar>
+        <a style={{ color: 'inherit', textDecoration: 'inherit'}} href="/"><Heading level='3' margin='none'>HMS</Heading></a>
+        </AppBar>
+
+        <Box
+          fill
+          align="center"
+          justify="top"
+          pad="medium">
+          <Box
+            width="medium"
+            pad="medium"></Box>
 }
