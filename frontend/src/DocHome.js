d2d4e1d412d692189eebee4204f6ace26c14fb53
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-08-31 19:51:13 -0400

    Creating sidebar buttons

--- frontend/src/DocHome.js
+++ frontend/src/DocHome.js
@@ -21,3 +21,51 @@ const theme = {
         },
     },
 };
+const SidebarButton = ({ label, ...rest }) => (
+  <Button plain {...rest}>
+      {({ hover }) => (
+          <Box
+              background={hover ? "#DADADA" : undefined}
+              pad={{ horizontal: "large", vertical: "medium" }}
+          >
+              <Text size="large">{label}</Text>
+          </Box>
+      )}
+  </Button>
+);
+
+const SidebarButtons = () => {
+  const [active, setActive] = useState();
+  return (
+      <Grommet full theme={theme}>
+          <Box fill direction="row">
+              <Box background="brand">
+                  {["Appointments", "View Patients", "Settings", "Sign Out"].map(label => (
+                      <SidebarButton
+                          key={label}
+                          label={label}
+                          active={label === active}
+                          onClick={() => {
+                              if (label === "Appointments") {
+                                  window.location = "/ApptList"
+                              }
+                              else if (label === "Sign Out") {
+                                  fetch("http://localhost:3001/endSession");
+                                  window.location = "/"
+                              }
+                              else if (label === "Settings") {
+                                  window.location = "/DocSettings"
+                              }
+                              else if (label === "View Patients") {
+                                  window.location = "/MedHistView"
+                              }
+                              setActive(label);
+                          }}
+                      />
+                  ))}
+
+              </Box>
+          </Box>
+      </Grommet>
+  );
+};
