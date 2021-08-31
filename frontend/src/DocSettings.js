e0602e38965af574c32c9fe21e9e219efd0ac4c2
Mona Waseem <monaqwaseem@gmail.com>
2021-08-31 21:32:57 +0000

    Create Doc setting component

---
+++ frontend/src/DocSettings.js
@@ -0,0 +1,30 @@
+import React, { Component} from 'react';
+
+import {
+    Box,
+    Button,
+    Heading,
+    Grommet,
+    FormField,
+    Form
+} from 'grommet';
+
+import './App.css';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default DocSettings;
