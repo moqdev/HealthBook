226f47f60f308d85a23a20aaeaf240228329e10d
Mona Waseem <monaqwaseem@gmail.com>
2021-08-31 20:23:28 +0000

    Update backend patient login functionality

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,31 @@ app.use('/api/users', usersRouter(dbHelpers));
 
 //Patient log-in to show if px is logged in
 app.get('/checklogin', (req, res) => {
+  let psql_statement = `SELECT * FROM Patient 
+                       WHERE email="${email}" 
+                       AND password="${password}"`;
+  console.log(psql_statement);
+  con.query(psql_statement, function (error, results, fields) {
+    if (error) {
+      console.log("error");
+      return res.status(500).json({ failed: 'error ocurred' })
+    }
+    else {
+      if (results.length === 0) {
+      } else {
+        var string = JSON.stringify(results);
+        var json = JSON.parse(string);
+        email_in_use = email;
+        password_in_use = password;
+        who = "pat";
+      }
+      return res.json({
+        data: results
+      })
+    };
+  });
+});
+  
 }
 
 
