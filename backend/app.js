53972b8c8470126bb825f68430a9dfbdfaa2cad6
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-09-01 14:19:17 -0400

    API to reset doctor password

--- backend/app.js
+++ backend/app.js
@@ -230,6 +230,28 @@ app.post('/resetPasswordPatient', (req, res) => {
     };
   });
 });
+//Resets Doctor Password
+app.post('/resetPasswordDoctor', (req, res) => {
+  let something = req.query;
+  let email = something.email;
+  let oldPassword = "" + something.oldPassword;
+  let newPassword = "" + something.newPassword;
+  let statement = `UPDATE Doctor
+                   SET password = "${newPassword}" 
+                   WHERE email = "${email}" 
+                   AND password = "${oldPassword}";`;
+  console.log(statement);
+  con.query(statement, function (error, results, fields) {
+    if (error) throw error;
+    else {
+      return res.json({
+        data: results
+      })
+    };
+  });
+});
+
+
 
 
 
