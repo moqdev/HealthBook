816b0839a6bf1ef9fcc721da0b19ee9c5d975bdf
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-09-01 11:56:55 -0400

    creating view for diagnoses

--- frontend/src/ShowDiagnoses.js
+++ frontend/src/ShowDiagnoses.js
@@ -24,7 +24,53 @@ const theme = {
     },
 };
 
+var id;
 
+export class ShowDiagnoses extends Component {
+    constructor(props) {
+        super(props);
+        id = props.match.params.id;
+    }
+    state = { diagnoses: [] }
+    componentDidMount() {
+        fetch('http://localhost:3001/showDiagnoses?id='+ id)
+        .then(res => res.json())
+        .then(res => this.setState({ diagnoses: res.data }));
+    }
+    render() {
+        const { diagnoses } = this.state;
+        const Header = () => (
+            <Box
+                tag='header'
+                background='brand'
+                pad='small'
+                elevation='small'
+                justify='between'
+                direction='row'
+                align='center'
+                flex={false}
+            >
+                <a style={{ color: 'inherit', textDecoration: 'inherit'}} href="/"><Heading level='3' margin='none'>HMS</Heading></a>
+            </Box>
+        );
+        const Body = () => (
+            <div className="container">
+                <div className="panel panel-default p50 uth-panel">
+                   
+                </div>
+                <hr />
+            </div>
+        );
+        return (
+            <Grommet full={true} theme={theme}>
+                <Box fill={true}>
+                    <Header />
+                    <Body />
+                </Box>
+            </Grommet>
+        );
+    }
+}
 
 
 export default ShowDiagnoses;
