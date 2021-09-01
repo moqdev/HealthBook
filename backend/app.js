727e59fdda16cdd63a6849aca028df2439aa2fed
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-09-01 14:40:00 -0400

    Error handling

--- backend/app.js
+++ backend/app.js
@@ -383,15 +383,21 @@ app.get('/MedHistView', (req, res) => {
     };
   });
 });
+// If 404, forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
-
-
-
-
-
-
+    // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 //Returns Appointment Info To patient logged In
@@ -591,7 +597,7 @@ app.get('/allDiagnoses', (req, res) => {
       })
     };
   });
-});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port} `);
