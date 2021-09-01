9cd46c37eb7c99e27b8f6b667e36a5ba3b034c37
Mona Waseem <monaqwaseem@gmail.com>
2021-09-01 15:21:01 +0000

    Add functionality to appointment scheduler

--- frontend/src/schedulingAppt.js
+++ frontend/src/schedulingAppt.js
@@ -71,4 +71,76 @@ const DropContent = ({ date: initialDate, time: initialTime, onClose }) => {
     onClose(date || initialDate, time || initialTime);
   };
 
+  return (
+    <Box align="center">
+      <Calendar
+        animate={false}
+        date={date || initialDate}
+        onSelect={setDate}
+        showAdjacentDays={false}
+        required
+      />
+      <Box flex={false} pad="medium" gap="small">
+        <Keyboard
+          required
+          onEnter={event => {
+            event.preventDefault(); // so drop doesn't re-open
+            close();
+          }}
+        >
+          <MaskedInput
+            mask={[
+              {
+                length: [1, 2],
+                options: [
+                  "0",
+                  "1",
+                  "2",
+                  "3",
+                  "4",
+                  "5",
+                  "6",
+                  "7",
+                  "8",
+                  "9",
+                  "10",
+                  "11",
+                  "12",
+                  "13",
+                  "14",
+                  "15",
+                  "16",
+                  "17",
+                  "18",
+                  "19",
+                  "20",
+                  "21",
+                  "22",
+                  "23",
+
+                ],
+                regexp: /^1[1-2]$|^[0-9]$/,
+                placeholder: "hh"
+              },
+              { fixed: ":" },
+              {
+                length: 2,
+                options: ["00"],
+                regexp: /^[0-5][0-9]$|^[0-9]$/,
+                placeholder: "mm"
+              }
+            ]}
+            value={time || initialTime}
+            name="maskedInput"
+            onChange={event => setTime(event.target.value)}
+            required
+          />
+        </Keyboard>
+        <Box flex={false}>
+          <Button label="Done" onClick={close} color="#00739D" />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 export default SchedulingAppt;
